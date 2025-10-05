f7d1bf38dc83ef8df333d5f573a185e669188a06
Vinicius José <viniciusjosedev@gmail.com>
2025-10-05 11:02:04 -0300

    inital project

---
+++ src/main/java/dev/vinion/vstream_java/vstream_java/VstreamJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.vinion.vstream_java.vstream_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VstreamJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VstreamJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vinion/vstream_java/vstream_java/VstreamJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vinion.vstream_java.vstream_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VstreamJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
